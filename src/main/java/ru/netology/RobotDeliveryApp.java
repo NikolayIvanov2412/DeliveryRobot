e3e0f732d806dc6d2a9432289f4e2d078cab24a5
Koljan <FbIPFbIP@yandex.ru>
2025-05-05 23:09:53 +0300

    Initial commit

--- src/main/java/ru/netology/RobotDeliveryApp.java
+++ src/main/java/ru/netology/RobotDeliveryApp.java
@@ -1,17 +1,75 @@
 package ru.netology;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class RobotDeliveryApp {
+
+    // Статическая карта для подсчета частот
+    public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        // Количество маршрутов
+        int numRoutes = 1000;
+
+        // Запускаем потоки для обработки маршрутов
+        for (int i = 0; i < numRoutes; i++) {
+            executor.submit(() -> processRoute(generateRoute("RLRFR", 100)));
         }
+
+        // Ждём завершение всех потоков
+        executor.shutdown();
+        try {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException ignored) {}
+
+        // Анализируем и выводим результат
+        analyzeAndPrintResult();
+    }
+
+    // Генерация маршрута
+    public static String generateRoute(String letters, int length) {
+        Random random = new Random();
+        StringBuilder route = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            route.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return route.toString();
+    }
+
+    // Обработка маршрута
+    public static void processRoute(String route) {
+        int countR = 0;
+        for (char c : route.toCharArray()) {
+            if (c == 'R') {
+                countR++; // считаем количество R
+            }
+        }
+
+        // Потокобезопасное обновление карты
+        synchronized (sizeToFreq) {
+            sizeToFreq.put(countR, sizeToFreq.getOrDefault(countR, 0) + 1);
+        }
+    }
+
+    // Анализ полученных данных и печать результата
+    public static void analyzeAndPrintResult() {
+        Map.Entry<Integer, Integer> mostCommonEntry = sizeToFreq.entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).orElse(null);
+
+        // Самое частое количество повторений
+        System.out.println("Самое частое количество повторений "
+                + mostCommonEntry.getKey() + " (встречалось "
+                + mostCommonEntry.getValue() + " раз)");
+
+        // Другие размеры
+        System.out.println("Другие размеры:");
+        sizeToFreq.entrySet().stream()
+                .filter(entry -> !entry.equals(mostCommonEntry))
+                .sorted(Map.Entry.comparingByKey())
+                .forEachOrdered(entry -> System.out.println("- " + entry.getKey() + " (" + entry.getValue() + " раз)"));
     }
 }
